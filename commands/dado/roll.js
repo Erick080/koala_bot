eaaaf73c24212bc7ca4b5e184a7a88b65d177079
erick <erickbranquinhomachado@gmail.com>
2024-01-11 15:27:32 -0300

    roll reutilizavel em outras partes do codigo

--- commands/dado/roll.js
+++ commands/dado/roll.js
@@ -1,6 +1,10 @@
 module.exports = {
   name: "roll",
   async execute(message, args) {
+    var res = await this.roll(args)
+    message.reply(res);
+  },
+  async roll(args) {
     const roll = args[args.length - 1].split("d");
     //definir a quantia e lados de dados
     //nr = Number of rolls
@@ -14,12 +18,12 @@ module.exports = {
       nr = 1;
     }
 
-    var def = 0;
+    var resultado = 0;
 
     for (let i = 1; i <= nr; i++) {
       const sides = Math.floor(Math.random() * ns + 1);
       lista.push(sides);
-      def += sides;
+      resultado += sides;
     }
 
     if (args.length > 2) {
@@ -31,24 +35,19 @@ module.exports = {
           lista = lista.sort((a, b) => b - a);
           break;
       }
-      def = lista[0];
+      resultado = lista[0];
     }
 
-    /*lista.forEach(element => {
-            def+=element
-        }); */
-
-    message.reply(
-      def +
-        "  " +
-        "<---" +
-        "  " +
-        "[" +
-        lista +
-        "]" +
-        "  " +
-        args[args.length - 1]
+    return (
+      resultado +
+      "  " +
+      "<---" +
+      "  " +
+      "[" +
+      lista +
+      "]" +
+      "  " +
+      args[args.length - 1]
     );
-    //console.log(lista)
   },
 };
