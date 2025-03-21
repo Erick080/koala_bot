27f4dde2065c26e722da4f18803617c451b41e97
Oscar Flávio Jr <oscarflaviojunior@gmail.com>
2025-03-21 14:06:09 -0300

    Mudança no sistema do gtn

--- commands/jogo/gtn.js
+++ commands/jogo/gtn.js
@@ -3,7 +3,7 @@ module.exports = {
   description: "Jogo de adivinhação de números",
   async execute(message) {
     let num = Math.floor(Math.random() * 100) + 1;
-    let tentativas = 10;
+    var tentativas = 10;
 
     await message.reply(
       `Vou pensar em um número entre 1 e 100. Tente adivinhar!
@@ -15,11 +15,12 @@ module.exports = {
     const collector = message.channel.createMessageCollector({
       filter,
       time: 60000,
+      max: tentativas,
     });
 
     collector.on("collect", (m) => {
       let tentativa = parseInt(m.content.trim());
-
+      let tentativasRestantes = tentativas - collector.collected.size;
       if (isNaN(tentativa) || tentativa < 1 || tentativa > 100) {
         return message.reply(
           "Por favor, digite um número válido entre 1 e 100."
@@ -30,21 +31,20 @@ module.exports = {
         message.reply("🎉 Parabéns! Você acertou! 🎉");
         collector.stop();
       } else {
-        tentativas--;
-        if (tentativas === 0) {
+        if (tentativasRestantes === 0) {
           message.reply(`❌ Suas tentativas acabaram! O número era ${num}.`);
           collector.stop();
         } else {
           let dica = tentativa < num ? "maior" : "menor";
           message.reply(
-            `O número que pensei é ${dica} que ${tentativa}. \nVocê tem ${tentativas} tentativas restantes. Digite um número:`
+            `O número que pensei é ${dica} que ${tentativa}. \nVocê tem ${tentativasRestantes} tentativas restantes. Digite um número:`
           );
         }
       }
     });
 
     collector.on("end", (_, reason) => {
-      if (reason !== "user" && tentativas > 0) {
+      if (reason !== "user" && tentativasRestantes > 0) {
         message.reply(`⏳ Tempo esgotado! O número era ${num}.`);
       }
     });
